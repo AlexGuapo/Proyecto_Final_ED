6482707697ec610ffdfb817ad6763418fcddd282
AlexGuapo <greyesduenas@gmail.com>
2025-06-06 23:05:02 -0600

    Modificacion de codigo en radix

--- radix_sort/radix_sort.py
+++ radix_sort/radix_sort.py
@@ -1,3 +1,10 @@
+import sys
+import os
+
+# Permitir importar desde bucket_sort
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'bucket_sort')))
+from bucket_sort import ordenar_bucket
+
 def contar_max_digitos(lista):
     #Devuelve el número máximo de dígitos en los números de la lista.
     if not lista:
@@ -5,23 +12,28 @@ def contar_max_digitos(lista):
     return len(str(max(lista)))
 
 def ordenar_radix(lista):
+    #Ordena una lista de enteros no negativos usando Radix Sort,delegando el ordenamiento interno de los buckets a Bucket Sort.
     max_digitos = contar_max_digitos(lista)
-    posicion = 1  # empezamos desde el dígito menos significativo
+    posicion = 1  # dígito menos significativo
 
     for _ in range(max_digitos):
-        # Crea 10 "buckets" para cada dígito (0 al 9)
+        # Crear 10 buckets para dígitos del 0 al 9
         buckets = [[] for _ in range(10)]
 
         for numero in lista:
             digito = (numero // posicion) % 10
             buckets[digito].append(numero)
 
-        # Reconstruye la lista uniendo los buckets
+        # Ordenar cada bucket usando bucket_sort
+        for bucket in buckets:
+            ordenar_bucket(bucket)
+
+        # Reconstruir lista original
         lista.clear()
         for bucket in buckets:
             lista.extend(bucket)
 
-        posicion *= 10  # pasa al siguiente dígito
+        posicion *= 10
 
 # Ejemplo de uso
 if __name__ == "__main__":
